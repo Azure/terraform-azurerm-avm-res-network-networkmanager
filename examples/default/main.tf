7927045800ffd0af4dd85a560babedc63e5cfa53
cshea15 <chashea@microsoft.com>
2023-12-07 16:22:49 -0500

    update scope

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -40,4 +40,7 @@ module "network_manager" {
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
   scope_accesses      = ["Connectivity", "SecurityAdmin"]
+  scope = {
+    
+  }
 }
